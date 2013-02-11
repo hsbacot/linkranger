69cfc0a078d4257d2ae9c2c761d509b94968c678
hsbacot <hsbacot@gmail.com>
2013-02-11 13:16:41 -0500

    user with devise and bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mylinks::Application.config.secret_token = '3ae0fed3eb037eb12b20345a03214d4a4ad8c58a9a29e043701c72a3a0efebffbca6fbea590490faa345c08990c31c73f967a9d70a762ca807bf4e3b3f301381'
